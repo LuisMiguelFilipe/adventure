d6ed1f7734743260b3e66ff3ea0470ae557a1675
Luis Filipe <papalagui@outlook.com>
2021-12-16 01:04:25 +0100

    vuex TS - take 1

--- fe/src/main.ts
+++ fe/src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import { createVueRouter } from "@/router";
-import App from "./App.vue";
+import { registerStore } from "@/store";
+
+import App from "@/App.vue";
 
 const routerInstance = createVueRouter();
 
 const app = createApp(App);
+
+registerStore(app);
 
 app.use(routerInstance);
 
